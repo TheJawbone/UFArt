7c19b12298862971ce2d54e822c9527e3ac396b0
Paweł Figiel <pawel.figiel8@gmail.com>
2018-11-13 08:21:18 +0100

    Commit before merge with master

---
+++ UFArt/Controllers/NavbarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace UFArt.Controllers
+{
+    public class NavbarController : Controller
+    {
+        public IActionResult Navbar()
+        {
+            return View("Navbar", "Hello");
+        }
+    }
+}
